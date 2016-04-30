9a52ed1a3b5d0ffc4cae8a38555abc5590a32e38
Matt McNeil <matthew.mcneil@gmail.com>
2016-04-30 12:46:54 -0700

    move flycheck config into after load hook

--- editors/spacemacs.d.target/matt/autoload/flycheck.el
+++ editors/spacemacs.d.target/matt/autoload/flycheck.el
@@ -1,16 +1,9 @@
-;; shell-mode
-(add-to-list 'flycheck-shellcheck-excluded-warnings "SC1090")
-(add-to-list 'flycheck-shellcheck-excluded-warnings "SC1091")
-(add-to-list 'flycheck-shellcheck-excluded-warnings "SC2034")
-
-;; https://gist.github.com/glasser/e8363df7640f2c445460#file-flycheck-top-window-el-L23
-(add-hook 'flycheck-error-list-after-refresh-hook
-          (lambda ()
-            (with-selected-window (flycheck-get-error-list-window t)
-              (text-scale-set -1)
-              (fit-window-to-buffer))))
-
 (with-eval-after-load 'flycheck
+  ;; shell-mode
+  (add-to-list 'flycheck-shellcheck-excluded-warnings "SC1090")
+  (add-to-list 'flycheck-shellcheck-excluded-warnings "SC1091")
+  (add-to-list 'flycheck-shellcheck-excluded-warnings "SC2034")
+
   (defconst flycheck-error-list-format
     [("Line" 4 flycheck-error-list-entry-< :right-align t)
      ("Col" 3 nil :right-align t)
@@ -24,4 +17,11 @@
                         ;; https://github.com/syl20bnr/spacemacs/issues/1097
                         (add-hook 'evil-normal-state-entry-hook 'company-abort)
                         )) t)
+
+  ;; https://gist.github.com/glasser/e8363df7640f2c445460#file-flycheck-top-window-el-L23
+  (add-hook 'flycheck-error-list-after-refresh-hook
+            (lambda ()
+              (with-selected-window (flycheck-get-error-list-window t)
+                (text-scale-set -1)
+                (fit-window-to-buffer))))
   )
