4d8f8abf5b0a1304b79931b4f37adc0f20b087c8
Matt McNeil <matthew.mcneil@gmail.com>
2016-05-11 02:57:05 -0700

    remove midnight-mode

--- editors/spacemacs.d.target/matt/deferred/history.el
+++ editors/spacemacs.d.target/matt/deferred/history.el
@@ -19,15 +19,3 @@
   (setq undohist-directory (expand-file-name "undo-history" spacemacs-cache-directory))
   :config
   (undohist-initialize))
-
-;;;;;;;;; midnight mode ;;;;;;;;;
-(ignore-errors ;; may not be available on all emacs versions
-  ;; https://www.emacswiki.org/emacs/MidnightMode
-  (require 'midnight)
-  (midnight-mode 1)
-  (setq midnight-period 43200) ;; run every 12 hours
-  )
-;; clean-buffer-list runs when midnight mode is triggered
-;; https://www.emacswiki.org/emacs/CleanBufferList
-;; clean up buffers that haven't been touched in 1 day
-(setq clean-buffer-list-delay-general 1)
