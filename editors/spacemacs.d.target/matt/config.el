e10a2815e0ebd4e1d5f6c40074db7dbe26eb3bf0
Matt McNeil <matthew.mcneil@gmail.com>
2016-09-19 19:48:33 -0700

    simplify path reference for personal config

--- editors/spacemacs.d.target/matt/config.el
+++ editors/spacemacs.d.target/matt/config.el
@@ -1,6 +1,6 @@
 (defun matt-load-deferred-config-files ()
   (message "loading config files from matt layer...")
   ;; load these after all other spacemacs layers have loaded
-  (mapc 'load (file-expand-wildcards "~/.emacs.d/private/matt/deferred/*.el"))
+  (mapc 'load (file-expand-wildcards "~/.spacemacs.d/matt/deferred/*.el"))
   ;; remove compile window
   )
