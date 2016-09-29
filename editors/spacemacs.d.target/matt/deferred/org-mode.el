97f2e22cf49acde9712a88a63253c07799fc168c
Matt McNeil <matthew.mcneil@gmail.com>
2016-09-28 22:48:20 -0700

    use proportional font in org buffers

--- editors/spacemacs.d.target/matt/deferred/org-mode.el
+++ editors/spacemacs.d.target/matt/deferred/org-mode.el
@@ -1,10 +1,15 @@
+;; (defun my-buffer-face-mode-variable ()
+;;   "Set font to a variable width (proportional) fonts in current buffer"
+;;   (interactive)
+;;   (buffer-face-mode))
+
 (with-eval-after-load 'org
   (setq-default org-startup-indented 1)
   ;; https://github.com/syl20bnr/spacemacs/issues/5474#issuecomment-195973923
   (setq org-planning-line-re "")
 
   (make-face 'org-mode-mellow-default)
-  (set-face-attribute 'org-mode-mellow-default nil :foreground "#6A6F7C")
+  (set-face-attribute 'org-mode-mellow-default nil :family "DejaVu Sans" :height 220 :foreground "#6A6F7C")
 
   (setq ispell-really-hunspell t)
   (spacemacs|diminish buffer-face-mode)
