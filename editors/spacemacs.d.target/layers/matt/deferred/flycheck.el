f60caa06768bf0b3f562c06a88dfa2e26106440d
Matt McNeil <matthew.mcneil@gmail.com>
2018-06-08 14:47:24 -0700

    fix flycheck error list

--- editors/spacemacs.d.target/layers/matt/deferred/flycheck.el
+++ editors/spacemacs.d.target/layers/matt/deferred/flycheck.el
@@ -6,11 +6,12 @@
   (global-flycheck-mode -1)
 
   (defconst flycheck-error-list-format
-    [("Line" 4 flycheck-error-list-entry-< :right-align t)
-     ("Col" 3 nil :right-align t)
-     ("Level" 8 flycheck-error-list-entry-level-<)
-     ("ID" 25 t)
-     ("Message" 0 t)]
+    `[("File" 8)
+      ("Line" 5 flycheck-error-list-entry-< :right-align t)
+      ("Col" 3 nil :right-align t)
+      ("Level" 8 flycheck-error-list-entry-level-<)
+      ("ID" 25 t)
+      (,(flycheck-error-list-make-last-column "Message" 'Checker) 0 t)]
     "Table format for the error list.")
 
   (add-hook 'flycheck-mode-hook
