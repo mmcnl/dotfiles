b5b1fbdd725ef6578fd4f51a72910aeeb065c0dd
Matt McNeil <matthew.mcneil@gmail.com>
2020-12-26 13:55:54 -0800

    update IA settings to sync petabox into nomad container

--- editors/dot_spacemacs.d/layers/matt/deferred/internet_archive.el
+++ editors/dot_spacemacs.d/layers/matt/deferred/internet_archive.el
@@ -1,9 +1,12 @@
 (defun sync-to-pod ()
   (when (string-match-p "petabox" (buffer-file-name))
+    (message "syncing %s to petabox" buffer-file-name)
     (shell-command-to-string
-     (format "/Users/matt/dev/ia/kre8/branch-sync-edit.sh -rlptzv --progress --exclude=.git --exclude=.vscode %s %s" buffer-file-name buffer-file-name)
+     (format "/Users/matt/dev/ia/nomad/vsync -rlptzv --progress --exclude=.git --exclude=.vscode %s %s" buffer-file-name buffer-file-name)
      )
     )
   )
-
 (add-hook 'after-save-hook 'sync-to-pod)
+
+(require 'browse-at-remote)
+(add-to-list 'browse-at-remote-remote-type-domains '("git.archive.org" . "gitlab"))
